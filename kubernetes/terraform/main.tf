adda3c5c10d13190fed14759eb75a31d044649b9
zmd73 <vladz1973@yandex.ru>
2022-08-25 21:26:36 +0200

    add files kubernetes-2

--- kubernetes/terraform/main.tf
+++ kubernetes/terraform/main.tf
@@ -27,7 +27,7 @@ resource "yandex_vpc_subnet" "kube-subnet" {
   name           = "kube-subnet"
   zone           = var.zone
   network_id     = yandex_vpc_network.kube-network.id
-  v4_cidr_blocks = ["10.244.0.0/16"]
+  v4_cidr_blocks = ["192.168.10.0/24"]
 }
 
 resource "yandex_compute_instance" "k8s_master" {
